dce94654edad5c35228e631422b0707fc9a30723
Haziel <haziel635@gmail.com>
2025-08-02 12:31:35 -0600

    Agregando la funcion para obtener los nombre y guardarlos en la lista, tambien funcion para recorrer la lista y mostrar los nombres en lista

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigos = [];
+
+function agregarAmigo() {
+    // Obtener el valor del input y agregarlo a la lista de amigos
+    let nombre = document.getElementById('amigo').value;
+    // Validar que el nombre no esté vacío
+    if (nombre) {
+        nombreAmigos.push(nombre);
+        document.getElementById('amigo').value = '';
+        mostrarListaAmigos();    
+    }else{
+        // Si el nombre está vacío, mostrar un mensaje de alerta
+        alert('Por favor, ingresa un nombre válido.');
+    }
+}
+
+function mostrarListaAmigos() {
+    // Mostrar la lista de amigos en el elemento <ul> con id 'listaAmigos'
+    let lista = document.getElementById('listaAmigos');
+    // Limpiar la lista antes de agregar los nuevos amigos
+    lista.innerHTML = '';
+    // Recorrer el array de amigos y crear un elemento <li> para cada uno
+    for(let i = 0; i < nombreAmigos.length; i++) {
+        // Crear un nuevo elemento <li> y agregarlo a la lista
+        let li = document.createElement('li');
+        // Asignar el nombre del amigo al contenido del <li>
+        li.textContent = nombreAmigos[i];
+        // Agregar el <li> al elemento <ul>
+        lista.appendChild(li);
+    }
+}
+
